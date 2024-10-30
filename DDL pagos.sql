fff95d250781093e84769776ee60a40e427cebc6
Alejandro-iu <chavezluisalejandro3@gmail.com>
2024-10-29 22:09:43 -0500

    Delete DDL pagos.sql

--- DDL pagos.sql
+++
@@ -1,8 +0,0 @@
--- Selecciona todos los atributos de los proveedores
-DELIMITER //
-CREATE PROCEDURE procShowPagosDDL()
-BEGIN
-	select pag_id, pag_monto, pag_fecha, pag_metodo_pago
-    from tbl_pagos;
-END//
-DELIMITER ;
