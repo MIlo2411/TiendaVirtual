24f42427f95c2bc04bd18b40ea7cebb83d8fe0e8
Alejandro-iu <chavezluisalejandro3@gmail.com>
2024-10-29 22:09:56 -0500

    Delete DDL productos.sql

--- DDL productos.sql
+++
@@ -1,8 +0,0 @@
--- Selecciona todos los atributos de los proveedores
-DELIMITER //
-CREATE PROCEDURE procShowProductsDDL()
-BEGIN
-	select pro_id, pro_nombre, pro_descripcion, pro_precio, pro_stock
-    from tbl_productos;
-END//
-DELIMITER ;
