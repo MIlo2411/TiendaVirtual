a179b202daf10e85f224919b16741a7f10779430
Alejandro-iu <chavezluisalejandro3@gmail.com>
2024-11-13 15:06:43 -0500

    Delete DDL carrito.sql

--- DDL carrito.sql
+++
@@ -1,8 +0,0 @@
--- Selecciona todos los atributos de los proveedores
-DELIMITER //
-CREATE PROCEDURE procShowCarritoDDL()
-BEGIN
-	select car_id, car_cantidad
-    from tbl_carrito;
-END//
-DELIMITER ;
