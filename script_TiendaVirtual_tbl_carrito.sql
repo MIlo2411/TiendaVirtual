4da6335896883a4eceeaec0037de69d0cc473299
Alejandro-iu <chavezluisalejandro3@gmail.com>
2024-11-13 15:06:06 -0500

    Update script_TiendaVirtual_tbl_carrito.sql

--- script_TiendaVirtual_tbl_carrito.sql
+++ script_TiendaVirtual_tbl_carrito.sql
@@ -40,4 +40,12 @@ BEGIN
     DELETE FROM tbl_carrito WHERE car_id = v_id;
 END//
 DELIMITER ;
+-- Selecciona todos los atributos de los proveedores
+DELIMITER //
+CREATE PROCEDURE procShowCarritoDDL()
+BEGIN
+	select car_id, car_cantidad
+    from tbl_carrito;
+END//
+DELIMITER ;
 
