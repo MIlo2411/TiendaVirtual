76e1190cfb569e885f7c4a7fd2c21ec732610b5e
Alejandro-iu <chavezluisalejandro3@gmail.com>
2024-11-13 15:09:07 -0500

    Update script_TiendaVirtual_tbl_pedidos.sql

--- script_TiendaVirtual_tbl_pedidos.sql
+++ script_TiendaVirtual_tbl_pedidos.sql
@@ -39,5 +39,12 @@ BEGIN
     DELETE FROM tbl_pedidos WHERE ped_id = v_id;
 END//
 DELIMITER ;
-
+-- Selecciona todos los atributos de los proveedores
+DELIMITER //
+CREATE PROCEDURE procShowPedidosDDL()
+BEGIN
+	select ped_id, ped_fecha, ped_estado, ped_total
+    from tbl_pedidos;
+END//
+DELIMITER ;
 
