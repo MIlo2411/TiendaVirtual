b5d19a4c311b5ade781e96eac774b99964b67423
Alejandro-iu <chavezluisalejandro3@gmail.com>
2024-11-13 15:15:28 -0500

    Update script_TiendaVirtual_tbl_clientes.sql

--- script_TiendaVirtual_tbl_clientes.sql
+++ script_TiendaVirtual_tbl_clientes.sql
@@ -18,19 +18,26 @@ DELIMITER ;
 
 -- ACTUALIZAR
 DELIMITER //
-CREATE PROCEDURE procUpdateClient(IN v_id INT, IN v_nombre VARCHAR(45),
- IN v_apellido VARCHAR(45), IN v_correo TEXT, IN v_contrasena TEXT,
- IN v_direccion TEXT, IN v_telefono INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `procUpdateClient`(
+    IN v_id INT,
+    IN v_nombre VARCHAR(45),
+    IN v_apellido VARCHAR(45),
+    IN v_correo TEXT,
+    IN v_contrasena TEXT,
+    IN v_direccion TEXT,
+    IN v_telefono INT
+    
+)
 BEGIN
     UPDATE tbl_clientes
-    SET cli_nombre = v_nombre, cli_apellido = v_apellido,
-    cli_correo = v_correo, cli_contrasena = v_contrasena, 
-    cli_direccion_envio = v_direccion, cli_telefono = v_telefono
+    SET cli_nombre = v_nombre,
+        cli_apellido = v_apellido,
+        cli_correo = v_correo,
+        cli_contrasena = v_contrasena,
+        cli_direccion_envio = v_direccion,
+        cli_telefono = v_telefono
     WHERE cli_id = v_id;
-END//
-DELIMITER ;
-
--- MOSTRAR
+END
 
 DELIMITER //
 CREATE PROCEDURE procShowClients()
