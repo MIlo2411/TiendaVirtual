b1ad4a4651ba544c7662cda3076a40d65d5ae37b
Alejandro-iu <chavezluisalejandro3@gmail.com>
2024-11-13 15:09:28 -0500

    Delete DDL pedidos.sql

--- DDL pedidos.sql
+++
@@ -1,8 +0,0 @@
--- Selecciona todos los atributos de los proveedores
-DELIMITER //
-CREATE PROCEDURE procShowPedidosDDL()
-BEGIN
-	select ped_id, ped_fecha, ped_estado, ped_total
-    from tbl_pedidos;
-END//
-DELIMITER ;
